087e0121803aa77e300a584655bdfff1814b3189
Parv3213 <parv3213@gmail.com>
2022-10-19 14:38:08 +0530

    Add empty components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import { Hero, Navbar, Stats, Business, Billing, CardDeals, Testimonials, Clients, Footer } from './components'
+import styles from './styles'
 
-function App() {
-  return <div className="App"></div>
+const App = () => {
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats /> <Business /> <Billing /> <CardDeals /> <Testimonials /> <Clients /> <Footer />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/CardDeals.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const CardDeals = () => {
+  return <div>CardDeals</div>
+}
+
+export default CardDeals
