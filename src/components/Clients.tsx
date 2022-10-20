88fb8d3529489d3a4385b93da465383b38fb027c
Parv3213 <parv3213@gmail.com>
2022-10-21 02:18:27 +0530

    Fix clients

--- src/components/Clients.tsx
+++ src/components/Clients.tsx
@@ -5,7 +5,7 @@ const Clients = () => {
   return (
     <section id="clients" className="flex flex-wrap justify-center items-center">
       {clients.map((client) => (
-        <div key={client.id} className="flex-1 flex justify-center items-center">
+        <div key={client.id} className="flex-1 flex justify-center items-center sm:min-w-[192px] min-w-[120px] m-5">
           <img src={client.logo} alt="client" className="sm:w-[192px] w-[100px] object-contain" />
         </div>
       ))}
